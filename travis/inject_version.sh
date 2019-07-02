b20c5d385d08d90b3971e29a8420807a27521d7b
Marquis Kurt <software@marquiskurt.net>
2019-07-01 20:08:08 -0400

    [travis] Remove parens from git hash/tag

--- travis/inject_version.sh
+++ travis/inject_version.sh
@@ -8,10 +8,10 @@ replacer="2.0.0"
 
 if [ ! -z $TRAVIS_TAG ]; then
   echo " ---> Tag exists, using tag."
-  sed -i "s/define config.version = \"$replacer\"/define config.version = \"$replacer ($TRAVIS_TAG)\"/g" mod/game/options.rpy;
+  sed -i "s/define config.version = \"$replacer\"/define config.version = \"$replacer.$TRAVIS_TAG\"/g" mod/game/options.rpy;
   cat mod/game/options.rpy | grep config.version;
 else
   echo " ---> Tag not exists, using hash."
-  sed -i "s/config.version = \"$replacer\"/config.version = \"$replacer ($TRAVIS_COMMIT)\"/g" mod/game/options.rpy;
+  sed -i "s/config.version = \"$replacer\"/config.version = \"$replacer.$TRAVIS_COMMIT\"/g" mod/game/options.rpy;
   cat mod/game/options.rpy | grep config.version;
 fi
