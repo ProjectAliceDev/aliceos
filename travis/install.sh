42efa6027f2263db381dab90ef791a00570ebab4
Marquis Kurt <software@marquiskurt.net>
2019-07-04 08:52:01 -0400

    [travis] Wrap renpy_version value in string

--- travis/install.sh
+++ travis/install.sh
@@ -6,7 +6,7 @@
 #  Created by Marquis Kurt on 7/4/19.
 #  Copyright © 2019 ProjectAliceDev. All rights reserved.
 
-renpy_version=$1
+renpy_version="$1"
 
 wget https://www.renpy.org/dl/$(renpy_version)/renpy-$(renpy_version)-sdk.tar.bz2
 tar xf renpy-$(renpy_version)-sdk.tar.bz2
