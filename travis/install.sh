b5b6b7d889477f1e7c98b0b33f9ec2d4e73020de
Marquis Kurt <software@marquiskurt.net>
2019-07-04 08:58:04 -0400

    [travis] Remove RPVER entirely in install.sh and interface params directly

--- travis/install.sh
+++ travis/install.sh
@@ -6,9 +6,7 @@
 #  Created by Marquis Kurt on 7/4/19.
 #  Copyright © 2019 ProjectAliceDev. All rights reserved.
 
-RPVER="$1"
-
-wget https://www.renpy.org/dl/$(RPVER)/renpy-$(RPVER)-sdk.tar.bz2
-tar xf renpy-$(RPVER)-sdk.tar.bz2
-rm renpy-$(RPVER)-sdk.tar.bz2
-mv renpy-$(RPVER)-sdk renpy
+wget https://www.renpy.org/dl/$1/renpy-$1-sdk.tar.bz2
+tar xf renpy-$1-sdk.tar.bz2
+rm renpy-$1-sdk.tar.bz2
+mv renpy-$1-sdk renpy
