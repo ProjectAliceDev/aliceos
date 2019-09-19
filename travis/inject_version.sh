b69978d7f31d89f5ec0ffa40163ee83635bd0b6b
Marquis Kurt <software@marquiskurt.net>
2019-09-19 17:50:28 -0400

    [.github/workflows + .travis] Do stuff
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- travis/inject_version.sh
+++ travis/inject_version.sh
@@ -14,8 +14,8 @@ if [ ! -z $TRAVIS_TAG ]; then
   cat mod/game/System/ASDefinitions.rpy | grep BUILD_ID;
 else
   echo " ---> Tag not exists, using hash."
-  sed -i "s/config.version = \"$replacer\"/config.version = \"$TRAVIS_COMMIT\"/g" mod/game/options.rpy;
-  sed -i "s/\"BUILD_ID\": \"GITHASH\"/\"BUILD_ID\":  \"$TRAVIS_COMMIT\"/g" mod/game/System/ASDefinitions.rpy;
+  sed -i "s/config.version = \"$replacer\"/config.version = \"${TRAVIS_COMMIT:0:9}\"/g" mod/game/options.rpy;
+  sed -i "s/\"BUILD_ID\": \"GITHASH\"/\"BUILD_ID\":  \"${TRAVIS_COMMIT:0:9}"/g" mod/game/System/ASDefinitions.rpy;
   cat mod/game/options.rpy | grep config.version;
   cat mod/game/System/ASDefinitions.rpy | grep BUILD_ID;
 fi
