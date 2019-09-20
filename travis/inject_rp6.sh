69373a4673bb86988a0d4b36d6a74790a12e03a8
Marquis Kurt <software@marquiskurt.net>
2019-09-20 14:41:53 -0400

    [.github + travis] Attach Ren'Py SDK version to config.version

--- travis/inject_rp6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-
-sed -i "s/build.package(build.directory_name + \"_ASBaseSystem\", 'zip', build.name, description=\"AliceOS Base System Distributable\")/build.package(build.directory_name + \"_ASBaseSystem-rp6\", 'zip', build.name, description=\"AliceOS Base System Distributable\")/g" mod/game/options.rpy;
-cat mod/game/options.rpy | grep build.package;
