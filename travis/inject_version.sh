5b2dec9ee6e19474283acce1435a46c43fba2010
Marquis Kurt <software@marquiskurt.net>
2019-09-20 13:42:52 -0400

    [aliceos + travis] Remove .travis.yml, change inject sequence

--- travis/inject_version.sh
+++ travis/inject_version.sh
@@ -3,7 +3,7 @@
 # You find yourself in a strange place. This is where we inject semvering via Git Tags
 # No need to change anything below the replacer variable.
 # Just change the value of replacer, the script will do everything for you
- 
+
 replacer="2.0.0"
 shortened_hash="$(echo $TRAVIS_COMMIT | head -c 7)"
 
@@ -14,15 +14,15 @@ if [[ $TRAVIS_TAG == "ref/tags"* ]]; then
 fi
 
 if [ ! -z $TRAVIS_TAG ]; then
-  echo " ---> Tag exists, using tag."
-  sed -i "s/define config.version = \"$replacer\"/define config.version = \"$TRAVIS_TAG\"/g" mod/game/options.rpy;
-  sed -i "s/\"BUILD_ID\": \"GITHASH\"/\"BUILD_ID\":  \"$TRAVIS_TAG\"/g" mod/game/System/ASDefinitions.rpy;
-  cat mod/game/options.rpy | grep config.version;
-  cat mod/game/System/ASDefinitions.rpy | grep BUILD_ID;
+  echo "Tag ${TRAVIS_TAG} found. Using tag..."
+  sed -i "s/define config.version = \"$replacer\"/define config.version = \"$TRAVIS_TAG\"/g" game/options.rpy;
+  sed -i "s/\"BUILD_ID\": \"GITHASH\"/\"BUILD_ID\":  \"$TRAVIS_TAG\"/g" game/System/ASDefinitions.rpy;
+  cat game/options.rpy | grep config.version;
+  cat game/System/ASDefinitions.rpy | grep BUILD_ID;
 else
-  echo " ---> Tag not exists, using hash."
-  sed -i "s/config.version = \"$replacer\"/config.version = \"$shortened_hash\"/g" mod/game/options.rpy;
-  sed -i "s/\"BUILD_ID\": \"GITHASH\"/\"BUILD_ID\":  \"$shortened_hash\"/g" mod/game/System/ASDefinitions.rpy;
-  cat mod/game/options.rpy | grep config.version;
-  cat mod/game/System/ASDefinitions.rpy | grep BUILD_ID;
+  echo "No tag found. Using Git hash..."
+  sed -i "s/config.version = \"$replacer\"/config.version = \"$shortened_hash\"/g" game/options.rpy;
+  sed -i "s/\"BUILD_ID\": \"GITHASH\"/\"BUILD_ID\":  \"$shortened_hash\"/g" game/System/ASDefinitions.rpy;
+  cat game/options.rpy | grep config.version;
+  cat game/System/ASDefinitions.rpy | grep BUILD_ID;
 fi
